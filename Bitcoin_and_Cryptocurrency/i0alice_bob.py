e8c26587c8e36e86c5950968d804d26195f6bb4e
greatabel <myreceiver2for2github@gmail.com>
2018-01-12 14:48:50 +0800

    trading

--- Bitcoin_and_Cryptocurrency/i0alice_bob.py
+++ Bitcoin_and_Cryptocurrency/i0alice_bob.py
@@ -2,7 +2,7 @@ from threading import Thread
 
 from time import sleep
 from termcolor import colored
-
+import random
 
 medicinePrice = 10
 toolPrice = 20
@@ -14,20 +14,33 @@ class TradingPeople(Thread):
         if name  == "alice":
             mesage += " I have medicines, I need tools. \n"
             self.medicineNum = 10
+            self.toolNum = 0
 
         elif name == "bob":
             mesage += " I have tools, I need medicines. \n" 
-            self.toolNum = 5           
+            self.medicineNum = 0
+            self.toolNum = 5    
+
         self.message = mesage
 
     def print_message(self):
         print(self.message)
 
     def offer_trade(self):
-        if hasattr(self, 'medicineNum'):
-            print("medicineNum:", self.medicineNum)
-        elif hasattr(self, 'toolNum'):
-            print("toolNum:", self.toolNum)
+        # if hasattr(self, 'medicineNum'):
+        print(self.name ,"'s medicineNum:", self.medicineNum)
+        # elif hasattr(self, 'toolNum'):
+        print(self.name, "'s toolNum:", self.toolNum)
+
+    def trading(self):
+        if hasattr(self, 'medicineNum') and hasattr(self, 'toolNum'):
+            if random.choice([True, False]):
+                self.medicineNum  -= 2
+                self.toolNum += 1
+            else:
+                self.medicineNum  += 2
+                self.toolNum -= 1               
+
 
     def run(self):
         print("Trading thread starting\n")
@@ -35,6 +48,7 @@ class TradingPeople(Thread):
         while( x < 10):
             self.print_message()
             self.offer_trade()
+            self.trading()
             sleep(1)
             x += 1
         print("Trading thread Ended\n")
