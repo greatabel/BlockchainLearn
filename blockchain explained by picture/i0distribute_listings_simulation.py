aa4bfd18f873d4411bf5846f8baa1cf2d3cacc0d
greatabel <myreceiver2for2github@gmail.com>
2018-01-29 16:41:18 +0800

    add exit

--- blockchain explained by picture/i0distribute_listings_simulation.py
+++ blockchain explained by picture/i0distribute_listings_simulation.py
@@ -12,6 +12,9 @@ from termcolor import colored
 # 3. 去中心化的账本——由网内用户共同维护的，它是去中心化的。
 
 # 暂时模拟第1个要求大概，有空模拟其他的
+
+age_of_this_universe = 60  # unit:seconds
+
 def show(s,color='green'):
     print(colored(s, color, attrs=['reverse', 'blink']), now_time())
 
@@ -24,8 +27,9 @@ threshold = 0.5
 listings = {}
 notebook = []
 def consumer():
-    print("consumer is waiting")
-    while threading.activeCount() > 1:
+    myname = threading.currentThread().getName()
+    print(myname + " consumer is waiting")
+    while threading.activeCount() > 2:
         semaphore.acquire()
         print('consumer lock it, when threading.activeCount()= ' + str(threading.activeCount())
                 + ' len(listings)=' + str(len(listings)))
@@ -36,6 +40,10 @@ def consumer():
                 show("Consumer notify : consumed item number %s" %using_one)
                 notebook.append(listings[using_one])
                 show(notebook, "cyan")
+        print('consumer threading.activeCount()=', threading.activeCount())
+        if threading.activeCount() <= 2:
+            break
+    print('consumer is gone!')
 
 def mining():
     time.sleep(random.random() * 10)
@@ -54,26 +62,35 @@ def producer():
     del listings[myname]
     print ("producer " + myname + " is gone.")
 
-if __name__ == '__main__':
+def main():
+    start = time.time()
     block_scheduler = threading.Thread(target=consumer)
     block_scheduler.start()
-    while True:
-        minerThreads = []
+    global minerThreads 
+    minerThreads = []
+    while True:        
         minerNum = random.randint(0, 50)
         for i in range(1, minerNum):
             minerThreads.append(threading.Thread(target=producer))
 
-
         for miner in minerThreads:
             miner.start()
-
         
         for miner in minerThreads:
-            miner.join()
-        
-        show('- >'*20, 'magenta')
+            miner.join()        
+
         minerThreads = []
+        show('- >'*20, 'magenta')
+        end = time.time()
+        show('time= %s' %(end - start))
+        if (end - start) >= age_of_this_universe:
+            print('Bang!')
+            semaphore.release()
+            break
+
     block_scheduler.join()
 
-
-    print ("main program terminated")
+if __name__ == '__main__':
+    print (threading.currentThread().getName() + " main program started")
+    main()
+    print (threading.currentThread().getName() + " main program terminated")
