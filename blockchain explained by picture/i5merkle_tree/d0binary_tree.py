57bebb65ef3f2b4fe2de2d5793a87036315e6f89
greatabel <myreceiver2for2github@gmail.com>
2018-02-01 11:10:53 +0800

    d0binary_tree.py

---
+++ blockchain explained by picture/i5merkle_tree/d0binary_tree.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.val = val
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def get_root(self):
+        return self.root
