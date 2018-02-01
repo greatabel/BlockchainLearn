9a60c30c9bbe93eb229a000c5f3eadb6b9b587ce
greatabel <myreceiver2for2github@gmail.com>
2018-02-02 01:12:37 +0800

    update

--- blockchain explained by picture/i2hash_algorithm.py
+++ blockchain explained by picture/i2hash_algorithm.py
@@ -12,6 +12,15 @@ def add_validation_to_sn(source, salt='1@2#3%4^'):
     sha_str = sha(source, salt)    
     return sha_str 
 
-source = 'py创世模块demo开始了'
-r = add_validation_to_sn(source)
-print(r)
+if __name__ == "__main__":
+    source = 'py创世模块demo开始了'
+    r = add_validation_to_sn(source)
+    print(r)
+    print('#'*20)
+    sha1 = hashlib.sha256()
+    sha1.update(b'how to use md5 in python hashlib?')
+    print(sha1.hexdigest())
+    sha2 = hashlib.sha256()
+    sha2.update(b'how to use md5 in ')
+    sha2.update(b'python hashlib?')
+    print(sha2.hexdigest())
