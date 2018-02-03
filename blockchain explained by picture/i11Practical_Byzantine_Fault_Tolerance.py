dca78d5e330ce08a617aafdf72941545cae75715
greatabel <myreceiver2for2github@gmail.com>
2018-02-04 01:17:21 +0800

    gethering response result

--- blockchain explained by picture/i11Practical_Byzantine_Fault_Tolerance.py
+++ blockchain explained by picture/i11Practical_Byzantine_Fault_Tolerance.py
@@ -5,27 +5,46 @@ import time
 import random
 from termcolor import colored
 
+# 实用拜占庭容错算法PBFT
+# https://www.jianshu.com/p/1e2acd3cbd9f
+
 def show(s,color='green'):
     return colored(s, color, attrs=['reverse', 'blink'])
 
 class Starter(Thread):
-    def __init__(self, queue):
+    def __init__(self, queue, r_queue):
         Thread.__init__(self)
         self.queue = queue
+        self.response_queue = r_queue
 
     def run(self):
-        for i in range(20):
+        for i in range(10):
             item = 'move #east# 500km'
             self.queue.put(item)
             print ('Starter notify : item %s appended to queue by %s \n'\
                     % (item, self.name))
             time.sleep(0.2)
+        results = {}
+        while True:
+            r_item = self.response_queue.get()
+            if self.response_queue.qsize() == 0:
+                break
+            print('r_item:', r_item, self.response_queue.qsize())
+            if r_item in results:
+                results[r_item] += 1
+            else:
+                results[r_item] = 1
+            self.response_queue.task_done()
+        # PBFT算法的核心理论是n>=3f+1
+        print(results)
+
 
 
 class CmdExecutor(Thread):
-    def __init__(self, queue):
+    def __init__(self, queue, r_queue):
         Thread.__init__(self)
         self.queue = queue
+        self.response_queue = r_queue
 
     def run(self):
         while True:
@@ -33,19 +52,25 @@ class CmdExecutor(Thread):
             tid = int(threading.currentThread().getName()[-1])
             myrandom = random.randint(1, 5)
             if tid % myrandom == 0:
-                item = show(item.replace("#east#", "#west#"))
-            print ('CmdExecutor execute :', item )
-
+                item = item.replace("#east#", "#west#")
+                showitem = show(item)
+                print ('CmdExecutor execute :', showitem )
+            else:
+                print ('CmdExecutor execute :', item )
             self.queue.task_done()
+            response_item = item.replace("move", "moved")
+
+            self.response_queue.put(response_item)
 
 
 if __name__ == '__main__':
         queue = Queue()
-        t1 = Starter(queue)
+        r_queue = Queue()
+        t1 = Starter(queue, r_queue)
         t1.start()
         CmdExecutorList = []
         for i in range(5):
-            t = CmdExecutor(queue)
+            t = CmdExecutor(queue, r_queue)
             CmdExecutorList.append(t)
 
         for t in CmdExecutorList:
