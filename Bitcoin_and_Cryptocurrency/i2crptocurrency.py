f9a008729fb2e52dff9716dbc662464bd8b86c96
greatabel <myreceiver2for2github@gmail.com>
2018-02-04 22:48:58 +0800

    i2crptocurrency.py

---
+++ Bitcoin_and_Cryptocurrency/i2crptocurrency.py
@@ -0,0 +1,25 @@
+# David Chaum 1983 提出加密技术运用于现金
+from Crypto.Signature import pss
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+# sender can create the signatue of a message using their private key
+message = b'To be signed'
+key = RSA.importKey(open('../blockchain explained by picture/i4PyCryptodome/d3private.pem').read())
+h = SHA256.new(message)
+signature = pss.new(key).sign(h)
+
+print(signature, type(signature))
+
+print('-' * 20)
+
+key = RSA.importKey(open('../blockchain explained by picture/i4PyCryptodome/d3public.pem').read())
+h = SHA256.new(message)
+verifier = pss.new(key)
+try:
+    verifier.verify(h, signature)
+    print("The signature is authentic.")
+except (ValueError, TypeError):
+    print("The signature is not authentic.")
+
